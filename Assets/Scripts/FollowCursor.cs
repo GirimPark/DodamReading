991150b595fa85301d01196b93030400cdec07e0
hyejeong99 <g2040g@naver.com>
2022-04-11 18:35:55 +0900

    interaction
    인터랙션 게임 추가

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class FollowCursor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //마우스 올렸을 때
+        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
