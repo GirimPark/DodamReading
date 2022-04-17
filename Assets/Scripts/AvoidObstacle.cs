2453f9a4c9eb890292c26ab523ba5554a06a0bb2
hyejeong99 <g2040g@naver.com>
2022-04-17 20:47:05 +0900

    인터랙션 추가
    인터랙션 끝

--- Assets/Scripts/AvoidObstacle.cs
+++ Assets/Scripts/AvoidObstacle.cs
@@ -7,148 +7,50 @@ using TMPro;
 
 public class AvoidObstacle : MonoBehaviour
 {
-    bool isArriveTrue;//도착지랑 위치 맞았는지
-    bool isWithPrince;//왕자랑 같이 왔는지
-    float distance;
+    public bool isArriveTrue;//공주 도착지랑 위치 맞았는지
+    public bool princeArriveTrue;//왕자 도착지랑 위치 맞았는지
+    public bool isWithPrince;//왕자랑 같이 왔는지
+    public float distance;
 
     public GameObject arrivePosition;//도착 오브젝트
     public Vector2 arrivePos;//도착지 위치
     public Vector2 princessPos;//공주 위치
     public Vector2 startPos;//시작 위치
 
-    public Text arriveText; //텍스트
-    //TextMeshProUGUI arriveText;
+    public GameObject princePosition;//왕자 오브젝트
+    public Vector2 princePos;//왕자 위치
+    public float distanceP;
 
-   /* [SerializeField] SpriteRenderer spriteRenderer;
-    public Vector3 LoadedPos;
-    float startPosx;
-    float startPosY;
-    bool isBeingHeld = false;
-    public bool isInLine;
-    float timelinePosY;
-
-    private void Start() 
-    {
-        LoadedPos = this.transform.position; 
-    }
-    private void Update() 
-    {
-        if(isBeingHeld)
-        {
-            Vector2 mousePos;
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            this.gameObject.transform.position = new Vector2(mousePos.x - startPosx, mousePos.y - startPosY);
-        }
-    }
-
-
-    #region 마우스 드래그앤 드롭
-
-
-
-    private void OnMouseDown() 
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-
-            spriteRenderer.color = new Color(1f, 1f, 1f, .5f);
-            Vector3 mousePos;
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-
-            startPosx = mousePos.x - this.transform.position.x;
-            startPosY = mousePos.y - this.transform.position.y;
-
-
-            isBeingHeld = true;
-
-        }
-    }
-
-    private void OnMouseUp() 
-    {
-        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
-        isBeingHeld = false;
-
-        if(isInLine)
-            this.gameObject.transform.position = new Vector3(this.gameObject.transform.localPosition.x, timelinePosY, -1f);
-        else 
-            this.gameObject.transform.position = LoadedPos;
-    }
-
-
-
-    #endregion
-    
-    
-    #region 타임라인이랑 맞는지
-    
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.CompareTag("TimeLine"))
-        {
-            isInLine = true;
-            timelinePosY = other.transform.position.y;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other) 
-    {
-        if(other.CompareTag("TimeLine"))
-        {
-            isInLine = false;
-        }
-    }
-
-
-    #endregion*/
 
     void Start(){
         isArriveTrue = false;//도착 위치에 맞는지
-        isWithPrince = false;//왕자랑 같이 왔는지
+        princeArriveTrue = false;//왕자 도착 위치에 맞는지
         startPos = transform.position;//공주 시작 위치 저장하기
-        //arriveText = GetComponent<TextMeshProUGUI>();
     }
     void Update()
     {
+        isWithPrince = GameObject.Find("prince").GetComponent<FollowPrincess>().closeDistance;//왕자랑 같이 있는지 받아오기
+        //도착지&공주 위치 업데이트
         arrivePos = arrivePosition.transform.position;
         princessPos = transform.position;
 
         distance = Vector2.Distance(princessPos, arrivePos);
 
-        /*if (isArriveTrue==false && distance<=0.5f)
+        //왕자&공주 위치 업데이트
+        princePos = princePosition.transform.position;
+        distanceP = Vector2.Distance(princePos, princessPos);
+
+        if (isArriveTrue==false && distance<=0.5f && isWithPrince==true)//도착지 도착했을 때
         {
             transform.position = arrivePos;//공주 위치랑 도착지 위치 맞춰줌
             isArriveTrue = true;
-        }
-        if(isArriveTrue==true){
-            //왕자랑 같이 왔을 때
-            if(isWithPrince==true){
-                arriveText.text="다음 게임으로 넘어가겠습니다.";
-            }
-            //왕자 안데리고 왔을 때
-            else if(isWithPrince==false){
-                //arriveText.text = resource.ToString();
-                arriveText.text="왕자를 데리고 오세요";
-                //transform.position = startPos;//위치 시작 위치로 바꿔주기
-            }
-        }*/
-        if (isArriveTrue==false && distance<=0.5f && isWithPrince==true)
-        {
-            //왕자를 데리고 왔을 때
-            transform.position = arrivePos;//공주 위치랑 도착지 위치 맞춰줌
-            isArriveTrue = true;
-            arriveText.text="다음 게임으로 넘어가겠습니다.";
-        }else if(distance<=0.5f && isWithPrince==false){
-            //왕자를 못데리고 왔을 때
-            arriveText.text="왕자를 데리고 오세요";
+            princeArriveTrue = true;
         }
     }
 
     void OnMouseDrag() 
     {
-        if (distance <= 0.5f && isWithPrince==true) return;
+        if (isArriveTrue == true && isWithPrince==true) return;
 
         Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeText : MonoBehaviour
+{
+    bool isWithPrince;//공주가 왕자 데리고왔을 때
+    bool isArriveTrue;//도착 지점에 도착했을 때
+    float distance;//공주-도착지점 거리
+    private TextMeshProUGUI textArrive;//변경 텍스트
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = GameObject.Find("princess").GetComponent<AvoidObstacle>().distance;
+        textArrive = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isWithPrince = GameObject.Find("princess").GetComponent<AvoidObstacle>().isWithPrince;
+        isArriveTrue = GameObject.Find("princess").GetComponent<AvoidObstacle>().isArriveTrue;
+
+        if(isWithPrince ==  true && isArriveTrue == true){
+            textArrive.text = "다음 게임으로 넘어가겠습니다.";//텍스트 변경
+            textArrive.color = Color.blue;//텍스트 색상 변경
+        }else{
+            textArrive.text = "왕자를 데리고 와주세요";//텍스트 변경
+            textArrive.color = Color.red;//텍스트 색상 변경
+        }
+    }
+}
