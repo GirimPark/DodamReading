3723fea58d2d9ee0b16ad3c58ed13daf8fcabb81
Suna1522 <71771248+k8749893@users.noreply.github.com>
2022-05-24 11:05:15 +0900

    OnOffWind 스크립트 추가

---
+++ Assets/Scripts/OnOffWind.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnOffWind : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject[] On = new GameObject[0];
+    public GameObject[] Off = new GameObject[0];
+    public GameObject[] destroy = new GameObject[0];
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnOffWind 실행중");
+
+        for(int i = 0; i < On.Length; i++)
+        {
+            On[i].SetActive(true);
+        }
+        for (int i = 0; i < Off.Length; i++)
+        {
+            Off[i].SetActive(false);
+        }
+        for (int i = 0; i < destroy.Length; i++)
+        {
+            Destroy(destroy[i]);
+        }
+        
+        
+    }
+}
