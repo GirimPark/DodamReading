a901055c8758171bdb51b9ba17ca8cedbab5fc17
hyejeong99 <g2040g@naver.com>
2022-05-25 10:00:55 +0900

    신 정지 추가

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -27,7 +27,15 @@ public class ChangeText : MonoBehaviour
         if(isArriveTrue == true){
             textArrive.text = "다음 게임으로 넘어가겠습니다.";//텍스트 변경
             textArrive.color = Color.blue;//텍스트 색상 변경
+            //1초 기다렸다가 다음 신으로 이동하기
+            StartCoroutine(WaitAndLoadScene()); 
+            IEnumerator WaitAndLoadScene()
+            {
+            yield return new WaitForSeconds(1.0f);
+
             SceneManager.LoadScene("M2_StoryScene");//신 변경
+            }
+        
         }else{
             textArrive.text = "왕자를 데리고 와주세요";//텍스트 변경
             textArrive.color = Color.red;//텍스트 색상 변경
---
+++ Assets/Scripts/CursorShow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorShow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = true;
+    }
+}
